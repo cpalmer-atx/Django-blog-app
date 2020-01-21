bd428fcc310079b193f32d8a8ef932863f7be0c3
Chad Palmer <chad.n.palmer@gmail.com>
2020-01-21 13:05:35 -0600

    database integration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone                                   # for Post.date_posted attribute (allows date modification)
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)    # If user deleted, that user's posts are deleted too
+
+    def __str__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
 # What we want the user to see when they are sent to this route
 # This is the logic for how we want to handle when a user goes to the blog page
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date': 'August 27, 2018'
-    },
+from django.shortcuts import render
+from .models import Post
+# from django.http import HttpResponse                  # used to implement dummy data below
 
-    {
-        'author': 'Chad',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date': 'August 28, 2018'
-    }
-]
+# Dummy data for testing before database is configured
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date': 'August 27, 2018'
+#     },
+
+#     {
+#         'author': 'Chad',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date': 'August 28, 2018'
+#     }
+# ]
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()                     # use 'posts': posts to implement dummy data above
     }
     return render(request, 'blog/home.html', context)
 
